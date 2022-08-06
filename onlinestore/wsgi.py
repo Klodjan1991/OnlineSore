9a750303d30e81135e8bea300c0783edc7131953
arismamo <arismamo@gmail.com>
2022-08-06 09:53:43 +0200

    Refactored project name

--- onlinestore/wsgi.py
+++ onlinestore/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for OnlineStore project.
+WSGI config for onlinestore project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'OnlineStore.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlinestore.settings')
 
 application = get_wsgi_application()
